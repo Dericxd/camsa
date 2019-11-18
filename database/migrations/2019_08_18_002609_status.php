e0ce9e9aa9025fa599345cb403d2a14985ba158f
Eduardo Martinez <eduarluis96@gmail.com>
2019-11-17 21:55:13 -0400

    Nuevos cambios

--- database/migrations/2019_08_18_002609_status.php
+++ database/migrations/2019_08_18_002609_status.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class Status extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('status', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->bigInteger('quantity');
-            $table->string('unity');
-            $table->string('presentation');
-            $table->string('status');
-            $table->bigInteger('amount');
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('status');
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([Rol::class]);
         $this->call([UsersTableSeeder::class]);
+        $this->call([StatusSeeder::class]);
     }
 }
